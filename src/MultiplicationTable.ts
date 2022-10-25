0ca5f7f7c6e9294e5a676ec1c02f8fd4987533d6
xma12 <xma12@slb.com>
2022-10-25 17:28:05 +0800

    feat: add multiplication table

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,46 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    if(!this.isValidInputs(start,end)){
+      return ""
+    }
+    else{
+      const multiTable = this.generateMultable(start,end);
+      return multiTable;
+    }
+  }
+  isValidInputs(start:number,end:number):boolean{
+    if(!this.isValidRange(start,end)||!this.isInputsInteger(start,end)){
+      return false
+    }
+    else{
+      return true
+    }
+
+  }
+  isValidRange(start:number,end:number):boolean{
+    const isValidStart = start<=10&&start>=0;
+    const isValidEnd = end<=10&&start>=0;
+    return isValidEnd&&isValidStart&&(start<=end);
+  }
+  isInputsInteger(start:number,end:number):boolean{
+    return Number.isInteger(start)&&Number.isInteger(end);
+  }
+  generateMultable(start:number,end:number):string{
+    let res = "";
+    for(let i = start;i<=end;i++){
+      res = i===end?res+this.generateRowString(start,i):res+this.generateRowString(start,i)+"\n"
+    }
+    return res;
+  }
+  generateRowString(rowStart:number,rowEnd:number):string{
+    let rowRes = "";
+    for(let j = rowStart;j<=rowEnd;j++){
+      rowRes =j===rowEnd? rowRes+this.generateCellString(j,rowEnd):rowRes+this.generateCellString(j,rowEnd)+"  "
+    }
+    return rowRes;
+  }
+  generateCellString(multiLeft:number,multiRight:number){
+    const cellRes = multiLeft
+    return `${multiLeft}*${multiRight}=${multiLeft*multiRight}`
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -4,13 +4,14 @@ describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+    const start = 2
+    const end = 4
 
     //when
     const rendered = table.render(start, end)
+    console.log(rendered);
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe(`2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16`)
   })
 })
