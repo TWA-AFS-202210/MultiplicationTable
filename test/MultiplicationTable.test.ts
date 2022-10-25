4fcce6079a5fb8f4059f4a32f15b25419c12bb2a
xma12 <xma12@slb.com>
2022-10-25 17:47:53 +0800

    feat: add unhappu path test

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,17 +1,30 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
+  it('should render multiplication table of (2, 4)', () => {
     // given
-    const table = new MultiplicationTable()
-    const start = 2
-    const end = 4
+    const table = new MultiplicationTable();
+    const start = 2;
+    const end = 4;
 
     //when
-    const rendered = table.render(start, end)
-    console.log(rendered);
+    const rendered = table.render(start, end);
 
     //then
     expect(rendered).toBe(`2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16`)
   })
+  it('should not render multiplication table of (4, 2)', () => {
+    // given
+    const table = new MultiplicationTable();
+    const start = 4;
+    const end = 2;
+
+    //when
+    const rendered = table.render(start, end);
+
+
+    //then
+    expect(rendered.length).toBe(0);
+  })
+  
 })
