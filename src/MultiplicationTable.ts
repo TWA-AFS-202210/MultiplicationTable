35928d764a99fdd2660e1b287872f6cc6c2f5f6d
Bo Zhao <BZhao7@slb.com>
2022-10-25 18:07:19 +0800

    feat: add create table and row

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,30 @@
-export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+export function multiplicationTable(start: number, end: number): string {
+  const isNotStartGreaterThanEnd = !isStartGreaterThanEnd(start,end)
+  const isValidate = isInRange(start, end)
+  if (isNotStartGreaterThanEnd&&isValidate) {
+    return createTable(start,end)
+  }else{
+    return ''
   }
+
+}
+
+export function isStartGreaterThanEnd(start: number, end: number) {
+  return start > end
+}
+export function isInRange(start: number, end: number): boolean {
+  const range = Array.from(Array(10).keys()).map(x => x+1)
+  return range.findIndex(element => element === start) > 0 && range.findIndex(element => element === end) > 0
+}
+
+export function createTable(start: number, end: number):string {
+  const rangeTable = Array.from({length:(end - start +1)},(value,index)=> start + index)
+  const tableMultipication:string[] = rangeTable.map((value,index)=> createRow(start,start+index) )
+  return tableMultipication.join('\n')
+}
+
+export function createRow(startRow: number, endRow: number):string {
+  const rangeRow = Array.from({length:(endRow - startRow +1)},(value,index)=> startRow + index)
+  const lineMultipication:string[] = rangeRow.map(num => num + '*' + endRow + '=' + endRow*num)
+  return lineMultipication.join('  ')
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,35 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import {createRow,createTable,multiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     // given
-    const table = new MultiplicationTable()
     const start = 1
     const end = 1
 
     //when
-    const rendered = table.render(start, end)
+    const rendered = multiplicationTable(start, end)
 
     //then
     expect(rendered).toBe('1*1=1')
   })
 })
+describe('Create a line', () => {
+  it('With 2 to 4 equal 2*4=8  3*4=12  4*4=16', () => {
+    const line:string = createRow(2,4)
+    expect(line).toBe('2*4=8  3*4=12  4*4=16')
+  })
+})
+
+describe('Create a Table', () => {
+  it('With 2 to 4 equal the given table', () => {
+    const table:string = createTable(2,4)
+    expect(table).toBe(`2*2=4
+2*3=6  3*3=9
+2*4=8  3*4=12  4*4=16`)
+  }),
+  it('With 2 to 2 equal the 2*2=4', () => {
+    const table:string = createTable(2,2)
+    expect(table).toBe('2*2=4')
+  })
+})
+
