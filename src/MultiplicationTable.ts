bf9cecf305767a26421800e010dd9a0fe5a6d177
Frank <zongpengchen@gmail.com>
2022-10-25 17:35:14 +0800

    add homework

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,41 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    if ((this.isStartNoGreaterThanEnd(start, end)) && (this.isRangeCorrect(start,end)))
+    {
+      console.log(this.multiplicationTableConstructor(start,end))
+      return this.multiplicationTableConstructor(start,end);
+    }
+    return ''
+  }
+
+  isStartNoGreaterThanEnd(start: number, end: number): boolean {
+    if (start <= end) return true;
+    else return false;
+  }
+
+  isRangeCorrect(start: number, end: number): boolean{
+    if (((start <= 10) && (end <= 10)) && ((start >= 1) && (end >= 1))) return true;
+    else return false;
+  }
+
+  multiplicationTableConstructor(start: number, end: number): string{
+    var multiplicationtable = new Array();
+    
+    for (var i=start; i<=end; i++)
+    {
+      var temp = new Array();
+      var sttemp = ''
+      for (var j=start; j<=i; j++){
+        temp.push(String(j) + '*' + String(i) + '=' + String(i*j));
+      }
+      
+      sttemp = temp.join('  ')
+      console.log(sttemp);
+      multiplicationtable.push(sttemp);
+    }
+    
+    return multiplicationtable.join('\n');
   }
 }
+
+
